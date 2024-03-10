45f3f2065a9d10206d13cd6ac2d0930075cacbbc
Alejandro Gonzalez <jose1999.369@hotmail.com>
2024-03-10 03:34:22 -0500

    anexo de while y switch en el main principal

--- Ejercicio_2/Main.java
+++ Ejercicio_2/Main.java
@@ -2,40 +2,57 @@ package Ejercicio_2;
 
 import java.util.Scanner;
 
-public class Main {public static void main(String[] args) {
-    int cantidadP=0;
-    String nombreP;
-    double preciop=0;
-    int cantP=0;
-    String resp="";
+public class Main {
+    public static void main(String[] args) {
+        int cantidadP = 0;
+        String nombreP;
+        double preciop = 0;
+        int cantP = 0;
+        String resp = "";
+        int op = 0;
 
-    Scanner leeScanner = new Scanner(System.in);
-    System.out.println("Bienvenido al sistema de facturacion");
-    System.out.println("Por favor ingrese la cantidad total de productos: ");
-    cantidadP = leeScanner.nextInt();
-    CuentaSupermercado cuenta = new CuentaSupermercado();
+        Scanner leeScanner = new Scanner(System.in);
+        CuentaSupermercado cuenta = new CuentaSupermercado();
 
-    for(int i = 0; i < cantidadP; i++){
-        System.out.println("Por favor ingrese el producto");
-        nombreP = leeScanner.next();
-        System.out.println("Por favor ingrese el precio del producto");
-        preciop = leeScanner.nextDouble();
-        System.out.println("Por favor ingrese la cantidad de ese producto");
-        cantP = leeScanner.nextInt();
+        while (op == 3) {
+            System.out.println("Bienvenido al sistema de facturacion");
+            System.out.println("Por favor seleccione una opcion del menu");
+            System.out.println("1) Ingresar producto");
+            System.out.println("2) Generar desprendible de facturacion");
+            System.out.println("3) Salir");
+            op = leeScanner.nextInt();
 
-        Producto producto = new Producto(nombreP, preciop);
-        cuenta.Agregar_productos(producto, cantidadP);
+            switch (op) {
+                case 1:
+                    System.out.println("Por favor ingrese la cantidad total de productos: ");
+                    cantidadP = leeScanner.nextInt();
 
-        System.out.println("¿Desea anexar otro producto? seleccione: Y/N");
-        resp = leeScanner.next();
-        if(resp.equals("N")){
-            break;
+                    for (int i = 0; i < cantidadP; i++) {
+                        System.out.println("Por favor ingrese el producto");
+                        nombreP = leeScanner.next();
+                        System.out.println("Por favor ingrese el precio del producto");
+                        preciop = leeScanner.nextDouble();
+                        System.out.println("Por favor ingrese la cantidad de ese producto");
+                        cantP = leeScanner.nextInt();
+
+                        Producto producto = new Producto(nombreP, preciop);
+                        cuenta.Agregar_productos(producto, cantidadP);
+
+                        System.out.println("¿Desea anexar otro producto? seleccione: Y/N");
+                        resp = leeScanner.next();
+                        if (resp.equals("N")) {
+                            break;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Desprendible de compra");
+                    cuenta.desprendible();
+                    break;
+            }
+
         }
+        leeScanner.close();
     }
-
-    System.out.println("Desprendible de compra");
-    cuenta.desprendible();
-
-    leeScanner.close();
-}
 }
