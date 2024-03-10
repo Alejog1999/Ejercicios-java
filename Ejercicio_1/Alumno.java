ea1b8ce8073010c2f4895cf1705ba95c99b27dfc
Alejandro Gonzalez <jose1999.369@hotmail.com>
2024-03-10 00:57:10 -0500

    Finalizacion de las clases con comentarios, metodos y funciones especificas

--- Ejercicio_1/Alumno.java
+++ Ejercicio_1/Alumno.java
@@ -11,53 +11,53 @@ public class Alumno {// Se define la clase en la cual se definen los objetos de 
     
 
     
-    LocalDate fechaActual = LocalDate.now();
+    LocalDate fechaActual = LocalDate.now();                                           //Importando la libreria time se puede extraer la fecha actual desde el computador
     DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     String fechaFormateada = fechaActual.format(formatoFecha);
 
-    public Alumno(String Nombre_del_alumno, String Apellido_del_alumno) {
+    public Alumno(String Nombre_del_alumno, String Apellido_del_alumno) { //Se definen las variables o propiedades del constructor
         this.Nombre_del_alumno = Nombre_del_alumno;
         this.Apellido_del_alumno = Apellido_del_alumno;
         this.asignaturas = new ArrayList<>();
     }
 
-    public void set_Nombre(String Nombre) {
+    public void set_Nombre(String Nombre) {                               //Metodo set para ingresar el nombre del alumno
         Nombre_del_alumno = Nombre;
     }
 
-    public void set_Apellido(String Apellido) {
+    public void set_Apellido(String Apellido) {                           //Metodo set para ingresar el apellido del alumno
         Apellido_del_alumno = Apellido;
     }
 
-    public String Get_nombre(){
+    public String Get_nombre(){                                           //Metodo get para extraer el nombre del alumno
         return Nombre_del_alumno;
     }
 
-    public String Get_apellido(){
+    public String Get_apellido(){                                         //Metodo get para extraer el apellido del alumno
         return Apellido_del_alumno;
     }
 
-    public List<Asignatura> Get_asignaturas(){
+    public List<Asignatura> Get_asignaturas(){                            //Metodo para listar y extraer las asignaturas anexadas
         return asignaturas;
     }
 
-    public void Agregar_asignatura(Asignatura asignatura) {
+    public void Agregar_asignatura(Asignatura asignatura) {               //metodo para anexar las asignaturas creando el objeto asignatura
         asignaturas.add(asignatura);
     }
 
-    public void Modificar_nota_alumno(String Nombre_a, double nota) {
-        for (Asignatura asignatura : asignaturas) {
-            if (asignatura.Get_materia().equals(Nombre_a)) {
-                asignatura.Set_nota(nota);
+    public void Modificar_nota_alumno(String Nombre_a, double nota) {     //Metodo para modificar la nota recorriendo las asignaturas por medio de un for hasta que encuentre la correcta
+        for (Asignatura asignatura : asignaturas) {                       //En caso de no encontrarla se muestra un mensaje de error 
+            if (asignatura.Get_materia().equals(Nombre_a)) {              //Por medio de un get se compara si es igual a la diligenciada
+                asignatura.Set_nota(nota);                                //Se da un set a la nueva nota
                 return;
             }
         }
         System.err.println("Error en la busqueda, por favor rectifique y vuelva a intentarlo");
     }
 
-    public void Get_asigntatura(String Nombre_asignatura) {
-        for (Asignatura asignatura : asignaturas) {
-            if (asignatura.Get_materia().equals(Nombre_asignatura)) {
+    public void Get_asigntatura(String Nombre_asignatura) {               //Metodo para extraer una asignatura especifica similar al metodo anterior
+        for (Asignatura asignatura : asignaturas) {                       //Se hace un recorrido de las asignaturas por medio de un for hasta encontrar
+            if (asignatura.Get_materia().equals(Nombre_asignatura)) {     //La asignatura deseada y con un mensaje de error en caso de no encontrar
                 System.out.println("Asignatura: " + asignatura.Get_materia() + "Nota: " + asignatura.Get_nota());
                 return;
             }
@@ -66,9 +66,9 @@ public class Alumno {// Se define la clase en la cual se definen los objetos de 
         System.err.println("Error en la busqueda, por favor rectifique y vuelva a intentarlo");
     }
 
-    public double promedio() {
-        double cantidad = 0;
-        int cont = 0;
+    public double promedio() {                                            //Funcion para retornar el promedio de las asignaturas creando una variable  
+        double cantidad = 0;                                              //de suma y otra de conteo para calcular el promedio, de igual manera se hace
+    int cont = 0;                                                         //se hace un recorrido por meido de un for
         if (asignaturas.isEmpty()) {
             return cantidad;
         } else {
@@ -80,20 +80,20 @@ public class Alumno {// Se define la clase en la cual se definen los objetos de 
         }
     }
 
-    public void Boletin() {
-        System.out.println("Boletin generado: "+ fechaFormateada);
-        System.out.println("Alumno: " + Nombre_del_alumno + " " + Apellido_del_alumno);
+public void Boletin() {                                                   //Metodo para extraer todas las asignaturas del alumno con un formato especifico                                            
+        System.out.println("Boletin generado: "+ fechaFormateada);        //Aqui se da uso a la fecha actual 
+        System.out.println("Alumno: " + Nombre_del_alumno + " " + Apellido_del_alumno); //Se anexa el nombre y apellido del alumno implicado
         System.out.println();
         System.out.println("Asignatura   Nota");
         System.out.println("------------------");
         // Iteramos sobre la lista de asignaturas e imprimimos el nombre y la nota de
         // cada una
-        for (Asignatura asignatura : asignaturas) {
+        for (Asignatura asignatura : asignaturas) {                       //Por medio de un bucle for se accede a cada asignatura y se muestra por consola
             System.out.printf("%-12s %.2f%n", asignatura.Get_materia(), asignatura.Get_nota());
         }
-        // Mostramos el promedio de las notas
+        
         System.out.println("------------------");
-        System.out.printf("Nota Media: %.2f%n", promedio());
+        System.out.printf("Nota Media: %.2f%n", promedio());      //Una vez terminado el bucle de muestra se calcula el promedio llamando a la funcion
         System.out.println("");
     }
 
--- Ejercicio_1/Main.java
+++ Ejercicio_1/Main.java
@@ -2,11 +2,11 @@ package Ejercicio_1;
 
 import java.util.Scanner;
 
-public class Main { // Metodo principal donde se ejecutan los comandos principales de consultar y
-                    // anexar
+public class Main {         // Metodo principal donde se ejecutan los comandos principales de consultar y anexar
+                            
     public static void main(String[] args) {
-        int leeer = 0;
-        Scanner leer = new Scanner(System.in);
+        int leeer = 0;                          //Se crea una variable encargada de leer la opcion dada por el usuario
+        Scanner leer = new Scanner(System.in);  //Se crea el objeto leer de la clase Scanner que permite la lectura por teclado
 
         System.out.println("Bienvenido al sistema, como primera instancia debe ingresar el nombre del alumno");
         System.out.println("Por favor ingrese el nombre del alumno");
@@ -14,10 +14,10 @@ public class Main { // Metodo principal donde se ejecutan los comandos principal
         System.out.println("Por favor ingrese el apellido del alumno");
         String apellido = leer.nextLine();
         Alumno alumno = new Alumno(nombre, apellido);
-        System.out.println("");
+        System.out.println("");               
         
 
-        while (leeer != 5) {
+        while (leeer != 5) {    //Se crea un while que siempre va estar ingresando en el menu a menos que se escoja salir
             System.out.println("Escoja una opcion valida");
             System.out.println("1) Anexar materias al alumno");
             System.out.println("2) Modificar Notas");
@@ -28,15 +28,14 @@ public class Main { // Metodo principal donde se ejecutan los comandos principal
             leeer = leer.nextInt();
             switch (leeer) {
                 case 1:
-                    if (alumno.Get_nombre() == "") {
+                    if (alumno.Get_nombre() == "") {    //Por medio de un get se consulta si verdaderamente hay un alumno 
                         break;
                     } else {
                         System.out.println("Ingrese el nombre de la asignatura");
-                        String asigna = leer.next(); // Consumir el carácter de nueva línea pendiente
+                        String asigna = leer.next(); 
                         System.out.println("Ingrese la nota de la asignatura");
                         double not = leer.nextDouble();
-                        System.out.println(alumno.Get_nombre() + " " + alumno.Get_apellido());
-                        alumno.Agregar_asignatura(new Asignatura(asigna, not));
+                        alumno.Agregar_asignatura(new Asignatura(asigna, not));     //Se llama el metodo para agregar asignaturas segun las diligencia el usuario
                         System.out.println("");
                     }
                     break;
@@ -46,21 +45,20 @@ public class Main { // Metodo principal donde se ejecutan los comandos principal
                     String asignatura = leer.next();
                     System.out.println("Ingrese la nota nueva");
                     double nota_nueva = leer.nextDouble();
-                    System.out.println(alumno.Get_nombre() + " " + alumno.Get_apellido());
-                    alumno.Modificar_nota_alumno(asignatura, nota_nueva);
+                    alumno.Modificar_nota_alumno(asignatura, nota_nueva); //Similar a la opcion anterior se diligencia el nombre de la asignatura y la nueva nota
                     System.out.println("");
                     break;
 
                 case 3:
                     alumno.Boletin();
-                    System.out.println("");
+                    System.out.println(""); //Se llama el metodo para mostrar el boletin
                     break;
 
                 case 4:
                     System.out.println("Ingrese el nombre de la asignatura a consultar");
                     String asignaturaa = leer.next();
                     System.out.println(alumno.Get_nombre() + " " + alumno.Get_apellido());
-                    alumno.Get_asigntatura(asignaturaa);
+                    alumno.Get_asigntatura(asignaturaa); //Metodo para consultar asignaturas especificas
                     System.out.println("");
                     break;
 
